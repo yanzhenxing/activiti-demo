52a59bf6d4ed5ceb28abec8ef0a0ecac534dd44f
yanzhenxing <yanzhenxing1234@126.com>
2015-09-09 21:29:35 +0800

    增加shiro

---
+++ activiti-demo/src/main/java/com/quauq/yanzhenxing/activiti/security/MyRealm.java
@@ -0,0 +1,27 @@
+package com.quauq.yanzhenxing.activiti.security;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * 自定义realm，负责认证和授权
+ * @author yanzhenxing
+ * @createDate 2015年9月9日
+ */
+public class MyRealm extends AuthorizingRealm {
+
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+		return null;
+	}
+
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+		return null;
+	}
+
+}
